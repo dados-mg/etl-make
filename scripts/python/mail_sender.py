499fcc037b2bbefaf1e950eb9938b4432da15b86
Gabriel Dornas <gabrielbdornas@gmail.com>
2021-10-22 15:35:51 -0300

    Check errors in logs/validate files See transparencia-mg/age7#189

--- scripts/python/mail_sender.py
+++ scripts/python/mail_sender.py
@@ -1,13 +1,20 @@
 import smtplib, os
+import json
 from pathlib import Path
 from dotenv import load_dotenv
 load_dotenv(dotenv_path=Path('.', '.env'))
 
+def check_log_erros():
+  if validate_erros_look_up():
+    send_mail()
+  else:
+    print('Nenhum erro detectado durante carga')
+
 def send_mail():
   sender_email = "etl.age7.ckan@gmail.com"
-  receiver_email = "gabrie.dornas@cge.mg.gov.com"
-  message = "Mensagem de Erro"
-
+  # receiver_email = "gabriel.dornas@cge.mg.gov.br"
+  receiver_email = "gabrielbdornas@gmail.com"
+  message = "Problema com a carga. Check os logs"
   smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
   smtpserver.ehlo()
   smtpserver.starttls()
@@ -17,4 +24,17 @@ def send_mail():
   smtpserver.sendmail(sender_email, receiver_email, message)
   print("E-mail enviado com sucesso. Confira sua caixa de entrada")
 
-send_mail()
+def validate_erros_look_up():
+  any_errors = False
+  path = 'logs/validate/'
+  folder = os.fsencode(path)
+  for file in os.listdir(folder):
+    filename = os.fsdecode(file)
+    file = open(f'{path}{filename}', "r").read()
+    validation = json.loads(file)
+    if validation["valid"] == False:
+      any_errors = True
+  return any_errors
+
+if __name__ == "__main__":
+  check_log_erros()

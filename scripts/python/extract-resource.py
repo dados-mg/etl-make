70fb207a450f1368034439ddc826a359b2836c4f
Francisco Alves <fjunior.alves.oliveira@gmail.com>
2021-11-26 18:16:52 -0300

    Altera make extract para determinar recursos a serem extraidos via datapackage.yaml
    Vide fa37c6003bad8a54f6acfa8245045d035ce04db4

--- scripts/python/extract-resource.py
+++ scripts/python/extract-resource.py
@@ -6,7 +6,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("resource_name", help="name of the resource to be extracted to data/staging/")
 args = parser.parse_args()
 
-dp = Package('datapackage.json')
+dp = Package('datapackage.yaml')
 
 resource = dp.get_resource(args.resource_name)
 

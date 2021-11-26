0fe0d033ed6359db0a6367582a24bea5687881aa
Francisco Alves <fjunior.alves.oliveira@gmail.com>
2021-11-26 17:22:40 -0300

    Fix DDL referencia chave estrangeira e insercao dados
    A chave estrangeira não pode referenciar uma coluna de tipo diferente (eg. BIGINT -> INT)

--- setup/provision_db.sh
+++ setup/provision_db.sh
@@ -4,3 +4,4 @@ mysql -uroot -proot < /vagrant/setup/age7.sql
 echo "Inserindo dados nas tabelas"
 mysql -uroot -proot < /vagrant/setup/dm_unidade_orc.sql
 mysql -uroot -proot < /vagrant/setup/ft_receita_v2018.sql
+mysql -uroot -proot < /vagrant/setup/ft_convenio_entrada.sql
